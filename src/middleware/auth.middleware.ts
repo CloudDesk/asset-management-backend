4264aafe5953e8077a821c19fe3946242c450077
suresh-on-cloud <sureshkumar@clouddesk.ae>
2026-01-02 12:34:56 +0530

    Update auth.middleware.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -53,7 +53,7 @@ export async function requireAuthentication(
     } else if (tokenFromQuery) {
       token = tokenFromQuery;
     }
-
+    console.log(token);
     if (!token) {
       logger.warn({
         ip: request.ip,
